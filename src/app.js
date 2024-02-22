6f5b795aa2223acf655aa49631f00728ddc715e7
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-02-22 18:08:32 +0700

    fix: timestamp

--- src/app.js
+++ src/app.js
@@ -1,48 +1,51 @@
-const clientId = "masukin client id";
+const clientId = "masukan client id";
 const DiscordRPC = require("discord-rpc");
 const RPC = new DiscordRPC.Client({ transport: "ipc" });
 
 // gambar
-const scream_cat = "https://media.tenor.com/qG3RhKKfngMAAAAS/fiamani-cat.gif";
-const scream_cat2 =
-  "https://media.tenor.com/yVB7cwslsHcAAAAC/cat-screaming.gif";
-const sadcat = "https://media.tenor.com/o9owbF_iALAAAAAC/sad-cat.gif";
-const kingflippynips =
-  "https://media.tenor.com/n9d7ic_UDMkAAAAC/flippynyps.gif";
+const gambar =
+  "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExYXQ3cXN2N3QyMnIwbGZ4bTdoOWJtZDhrMjZ6MnBlMmtveHl1Mm42eSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/SS8CV2rQdlYNLtBCiF/giphy.gif";
+
+const gambar2 = "https://media.tenor.com/Gi-2B1fmMcUAAAAi/intense-cat-money-bath.gif";
 // ----
 
 async function setActivity() {
   if (!RPC) return;
+
+  const currentTimestamp = Date.now();
+
   RPC.setActivity({
     details: "Life is so cute and adorable.",
     state: "It's so funny, I can't stop laughing.",
-    // startTimestamp: new Date(Date.now() - 720 * 1000000 ),
-
-    largeImageKey: kingflippynips,
+    startTimestamp: currentTimestamp,
+    largeImageKey: gambar,
     largeImageText: "Haha.",
-    // smallImageKey: "",
+    smallImageKey: gambar2,
     smallImageText: "Sucks.",
     type: 2,
     instance: false,
-    // buttons: [
-    //   // {
-    //   //   label: "PixiHive 🍔",
-    //   //   url: "https://dsc.gg/pixihive/",
-    //   // },
-    //   // {
-    //   //   label: "zeemarimo 🍔",
-    //   //   url: "https://www.youtube.com/@zeemarimo/",
-    //   // }
-    // ],
+    buttons: [
+      {
+        label: "PixiHive 🍔",
+        url: "https://dsc.gg/pixihive/",
+      },
+      {
+        label: "zeemarimo 🍔",
+        url: "https://www.youtube.com/@zeemarimoo/",
+      }
+    ],
   });
 }
 
 RPC.on("ready", () => {
   setActivity();
 
+  // update every 48 hours
   setInterval(() => {
     setActivity();
-  }, 2000);
+  }, 172800000); //172800000 milisecond = 48 hours
 });
 
 RPC.login({ clientId }).catch(console.error);
+
+
